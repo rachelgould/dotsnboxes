81f1c7c86aa52dccf4d48c298d4bf361d8aaa988
Rachel Gould <rachel@rachelgould.dev>
2021-03-28 12:15:04 -0700

    refactor

--- components/game/Board.vue
+++ components/game/Board.vue
@@ -11,17 +11,11 @@ export default {
   name: 'board',
   components: { Cell },
   data: (inst) => ({
-    gridClass: `w-80 h-80 grid grid-cols-${inst.$store.state.settings.columns} grid-rows-${inst.$store.state.settings.rows}`,
+    gridClass: `w-80 h-80 mx-auto mt-16 grid grid-cols-${inst.$store.state.settings.columns} grid-rows-${inst.$store.state.settings.rows}`,
   }),
   computed: {
     game() {
       return this.$store.state.board
-    },
-    rows() {
-      return this.$store.state.settings.rows
-    },
-    columns() {
-      return this.$store.state.settings.columns
     },
   },
 }
