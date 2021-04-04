2208be5d439b209d8d5682e6fc69bf69b301f8ee
Rachel Gould <rachel@rachelgould.dev>
2021-04-03 20:36:08 -0700

    styling

--- components/game/Game.vue
+++ components/game/Game.vue
@@ -1,7 +1,15 @@
 <template>
   <div>
-    <p>{{ getActivePlayer }}'s Turn!</p>
-    <base-button :click-action="startGame">Start Game</base-button>
+    <h1
+      class="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl lg:text-5xl xl:text-6xl mb-8"
+    >
+      <span class="block xl:inline">Dots</span>
+      <span class="block text-yellow-600 xl:inline">'n Boxes</span>
+    </h1>
+    <base-button :click-action="handleStartClick">
+      {{ started ? 'New Game' : 'Start Game' }}
+    </base-button>
+    <p class="m-6">{{ getActivePlayer }}'s Turn!</p>
     <div>
       <board />
     </div>
@@ -17,6 +25,9 @@ import Board from '@/components/game/Board'
 export default {
   name: 'game',
   components: { BaseButton, CirclePoint, Board },
+  data: () => ({
+    started: false,
+  }),
   computed: {
     ...mapGetters(['getActivePlayer']),
   },
@@ -24,6 +35,10 @@ export default {
     ...mapActions({
       startGame: 'initBoard',
     }),
+    handleStartClick() {
+      this.started = true
+      this.startGame()
+    },
   },
 }
 </script>
